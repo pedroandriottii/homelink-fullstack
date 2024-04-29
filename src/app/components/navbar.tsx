2d344bc06c7f347bfbc7e2210a3f8019810e22c1
Pedro Andriotti <phab@cesar.school>
2024-04-29 10:05:38 -0300

    Fixing underline bug on navbar. Adding whatsapp link

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -13,34 +13,7 @@ export default function Navbar() {
 
   return (
     <>
-      <style jsx>{`
-        .link {
-          position: relative;
-          color: white;
-          text-decoration: none;
-          padding-bottom: 5px; /* Adjust space for underline */
-        }
-
-        .link::after {
-          content: '';
-          position: absolute;
-          left: 0;
-          bottom: 0; /* Adjust to control distance from text */
-          height: 2px; /* Height of underline */
-          width: 100%;
-          background-color: white;
-          transform: scaleX(0);
-          transform-origin: bottom right;
-          transition: transform 0.3s ease-out;
-        }
-
-        .link:hover::after,
-        .link:focus::after {
-          transform: scaleX(1);
-          transform-origin: bottom left;
-        }
-      `}</style>
-      <header className="bg-home-link-background-blue">
+      < header className="bg-home-link-background-blue" >
         <div className="lg:hidden flex items-center justify-between pt-10 pl-6 pb-6 pr-6 w-full box-border">
           <button className="relative p-4 focus:outline-none" onClick={toggleMenu}>
             <div className="block w-8">
@@ -62,11 +35,11 @@ export default function Navbar() {
 
         {/* Navbar mobile */}
         <div
-          style={{ maxHeight: maxHeight }} className={`transition-max-height duration-700 ease-in-out overflow-hidden lg:hidden`}>
+          style={{ maxHeight: maxHeight }} className={`overflow-hidden lg:hidden`}>
           <div className="bg-home-link-background-blue w-full p-6">
-            <a href="/" className="link block py-2 border border-solid rounded-xl mb-3 pl-4 uppercase text-xl">Home</a>
-            <a href="/about" className="link block py-2 border border-solid rounded-xl mb-3 pl-4 uppercase text-xl">Portfólio</a>
-            <a href="/services" className="link block py-2 border border-solid rounded-xl mb-3 pl-4 uppercase text-xl">Nossos Serviços</a>
+            <a href="/" className=" text-white border-white link block py-2 border border-solid rounded-xl mb-3 pl-4 uppercase text-xl">Home</a>
+            <a href="/about" className=" text-white border-white link block py-2 border border-solid rounded-xl mb-3 pl-4 uppercase text-xl">Portfólio</a>
+            <a href="/services" className=" text-white border-white link block py-2 border border-solid rounded-xl mb-3 pl-4 uppercase text-xl">Nossos Serviços</a>
           </div>
         </div>
 
@@ -81,13 +54,13 @@ export default function Navbar() {
             />
           </a>
           <div className='flex gap-12'>
-            <a href="/" className="link">Home</a>
-            <a href="/about" className="link">Portfólio</a>
-            <a href="/services" className="link">Nossos Serviços</a>
-          </div> 
-          <a href="" className=' text-white font-bold font-sans inline-block border border-solid rounded-xl border-home-link-background-blue p-2 bg-home-link-seccond-blue hover:bg-home-link-light-blue hover:text-home-link-background-blue'>Solicitar Orçamento</a>
+            <a href="/" className="link text-white">Home</a>
+            <a href="/about" className="link text-white">Portfólio</a>
+            <a href="/services" className="link text-white">Nossos Serviços</a>
+          </div>
+          <a href="https://api.whatsapp.com/send?phone=5581988145903&text=Ol%C3%A1!%20Queria%20saber%20mais%20sobre%20os%20servi%C3%A7os%20de%20Automa%C3%A7%C3%A3o%20Residencial" target='__blank' className=' text-white font-bold font-sans inline-block border border-solid rounded-xl border-home-link-background-blue p-2 bg-home-link-seccond-blue hover:bg-home-link-light-blue hover:text-home-link-background-blue'>Solicitar Orçamento</a>
         </div>
-      </header>
+      </header >
     </>
   );
 }
